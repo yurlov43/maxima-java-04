5f44e9222954d33df6fa54859c1c13ecb8a71d7a
Vladimir Yurlov <yurlov43@gmail.com>
2022-12-02 01:56:53 +0300

    Connected to the Lombok project

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,10 +1,9 @@
 package org.example;
 
-public class App
-{
+public class App {
     public static void main( String[] args )
     {
-        Train train = new Train("Train", 30, 60, 6);
+        Train train = new Train("Train", 30, true);
         System.out.println(train.getName());
     }
 }
